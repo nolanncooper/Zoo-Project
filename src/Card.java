642bd58d0de4cb895364b75e5ed591c61f415fdf
nolanncooper <74671514+nolanncooper@users.noreply.github.com>
2021-02-22 20:17:33 -0700

    Create Card.java

---
+++ src/Card.java
@@ -0,0 +1,46 @@
+
+public class Card {
+
+	private String rank;
+	private String suit;
+	private int value;
+	
+	
+	public Card(String r, String s, int v) {
+		rank = r;
+		suit = s;
+		value = v;
+		
+	}
+
+
+	public String getRank() {
+		return rank;
+	}
+
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+
+	public String getSuit() {
+		return suit;
+	}
+
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+
+	public int getValue() {
+		return value;
+	}
+
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
