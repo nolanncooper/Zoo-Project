2f957c9f71d5b7bf6263c56a19ae5f35491a8f59
nolanncooper <74671514+nolanncooper@users.noreply.github.com>
2021-02-24 19:38:32 -0700

    Update Casino.java

--- src/Casino.java
+++ src/Casino.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 
 
 public class Casino {
-	public static void CasinoMain(int i) throws IOException {
+	public static void CasinoMain() throws IOException {
 		Scanner intInput = new Scanner(System.in);
 		Scanner stringInput = new Scanner(System.in);
-		int casinoZooBalance = i;
 		System.out.println("I see that you have decided to spend that cash...");
 		Speaker.speak("I see that you have decided to spend that cash...");
 		System.out.println();
-		System.out.println("You are currently sitting at: " + casinoZooBalance + " zoo dollars.");
-		Speaker.speak("You are currently sitting at:   " + casinoZooBalance + " zoo dollars");
+		System.out.println("You are currently sitting at: " + Main.zooBalance + " zoo dollars.");
+		Speaker.speak("You are currently sitting at:   " + Main.zooBalance + " zoo dollars");
 		System.out.println();
 		System.out.println("Let's take a look at some of the possible games you can play.");
 		Speaker.speak("Let's take a look at some of the possible games you can play.");
@@ -27,29 +26,46 @@ public class Casino {
 		SpaceAdder.addSpace();
 		if(input == 1) {
 			System.out.println("Score Hero it is");
+			Speaker.speak("Score Hero it is");
 			System.out.println();
 			System.out.println("You have the opportunity to earn 1000 dollars or lose 1000 dollars.");
+			Speaker.speak("You have the opportunity to earn 1000 dollars or lose 1000 dollars.");
 			System.out.println();
 			System.out.println();
 			System.out.println("Press enter to confim you would like to spend money on Score Hero...");
 			String snput = stringInput.nextLine();
-			ScoreHeroMain.ScoreHeroRun(casinoZooBalance);
+			ScoreHeroMain.ScoreHeroRun();
+			System.out.println();
+			System.out.println("Your new zoo balance is:  " + Main.zooBalance);
+			Speaker.speak("Your new zoo balance is:  " + Main.zooBalance);
 		}
 		if(input == 2) {
 			System.out.println("Skiball it is");
+			Speaker.speak("Skiball it is");
 			System.out.println();
+			System.out.println("You have the opportunity to earn 500 dollars or lose 500 dollars, depending on if you can get more than 100 points. ");
+			Speaker.speak("You have the opportunity to earn 500 dollars or lose 500 dollars.");
 			System.out.println();
 			System.out.println("Press press enter to confim you would like to spend money on Skiball...");
 			String snput = stringInput.nextLine();
 			SkiballMain.main();
+			System.out.println();
+			System.out.println("Your new zoo balance is:  " + Main.zooBalance);
+			Speaker.speak("Your new zoo balance is:  " + Main.zooBalance);
 		}
 		if(input == 3) {
 			System.out.println("War it is");
+			Speaker.speak("War it is");
 			System.out.println();
+			System.out.println("You have the opportunity to earn 750 dollars or lose 750 dollars.");
+			Speaker.speak("You have the opportunity to earn 750 dollars or lose 750 dollars.");
 			System.out.println();
 			System.out.println("Press press enter to confim you would like to spend money on War...");
 			String snput = stringInput.nextLine();
 			WarRunner.main();
+			System.out.println();
+			System.out.println("Your new zoo balance is:  " + Main.zooBalance);
+			Speaker.speak("Your new zoo balance is:  " + Main.zooBalance);
 		}
 		
 	}
